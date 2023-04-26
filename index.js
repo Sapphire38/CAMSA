33b36b1da09504862a0c68f028d1052f77286fcf
Juani <juanygr@hotmail.com>
2023-04-26 15:41:18 -0300

    update

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function cerrarModal(){
     modal.style.display = "none"
 }
 
+var i = 0;
+var txt = `Soluciones  rápidas` + ` y  de  calidad`;
+var speed = 50;
+function typeWriter() {
+    
+    if (i < txt.length) {
+      document.getElementById("demo").innerHTML += txt.charAt(i);
+       i++;
+      setTimeout(typeWriter, speed); 
+    }
+    // else{
+    //     setTimeout(function() {document.getElementById("demo").innerHTML = ""}, 3000)  
+    // }
+}
 
+  window.onload = function() {
+    typeWriter()
+  }
